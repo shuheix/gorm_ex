60e0668fd866037b3e8aeb98fec8ed83ce9ebbbf
shuheix <ono.shuhei210@gmail.com>
2024-08-19 20:31:28 +0900

    fix: migrate & create_sample

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
 func main() {
-	fmt.Println("Hello World")
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect db")
+	}
+
+	db.AutoMigrate(&Product{})
+
+	db.Create(&Product{Code: "D42", Price: 100})
 }

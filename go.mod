c1f091a40bd5283aff9a6cff9a31777088eb9d9c
shuheix <ono.shuhei210@gmail.com>
2024-08-19 20:55:30 +0900

    fix: pretty printいれた

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/shuheix/gorm_ex
 go 1.22.5
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/davecgh/go-spew/spew"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -17,7 +18,14 @@ func main() {
 		panic("failed to connect db")
 	}
 
-	db.AutoMigrate(&Product{})
+	//db.AutoMigrate(&Product{})
 
-	db.Create(&Product{Code: "D42", Price: 100})
+	//db.Create(&Product{Code: "D42", Price: 100})
+
+	// Read
+	var product Product
+	db.First(&product, 1)
+
+	// {{1 2024-08-19 20:18:52.661687111 +0900 +0900 2024-08-19 20:18:52.661687111 +0900 +0900 {0001-01-01 00:00:00 +0000 UTC false}} D42 100}
+	spew.Dump(product)
 }
